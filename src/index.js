8491dbbc4b46681be93305b046456650c965a5a8
Ryan Dabler <rpdabler@gmail.com>
2018-06-18 21:22:14 -0600

    create API

---
+++ src/index.js
@@ -0,0 +1,12 @@
+////////////////////
+// Initialize
+////////////////////
+const { typeOf } = require("tupos");
+const { duplicate } = require("./facsimile");
+
+////////////////////
+// Main
+////////////////////
+const copy = param => duplicate[typeOf(param)](param)
+
+module.exports = { copy };
