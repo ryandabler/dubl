15786319a53bd4762e37d1546cea1ac19c82d67a
Ryan Dabler <rpdabler@gmail.com>
2018-06-22 19:29:52 -0600

    rename duplicate to _duplicate

--- src/facsimile.js
+++ src/facsimile.js
@@ -65,7 +65,7 @@ const duplicateObject = obj => {
     const retObj = {};
 
     for (const key in obj) {
-        retObj[key] = duplicate[typeOf(obj[key])](obj[key]);
+        retObj[key] = _duplicate[typeOf(obj[key])](obj[key]);
     }
 
     return retObj;
@@ -83,7 +83,7 @@ const duplicateObject = obj => {
 const duplicateArray = arr => {
     const retArr = [];
     for (const key in arr) {
-        retArr[key] = duplicate[typeOf(arr[key])](arr[key]);
+        retArr[key] = _duplicate[typeOf(arr[key])](arr[key]);
     }
 
     return retArr;
@@ -137,7 +137,7 @@ const duplicateTypedArray = constructor => typedArr => {
 
     for (const key in typedArr) {
         newTypedArr[key] =
-            duplicate[typeOf(typedArr[key])](typedArr[key]);
+            _duplicate[typeOf(typedArr[key])](typedArr[key]);
     }
 
     return newTypedArr;
@@ -160,7 +160,7 @@ const duplicateArrayBuffer = arrBuff => new ArrayBuffer(arrBuff.length)
 const duplicateDataView = dv =>
     new DataView(dv.buffer, dv.byteOffset, dv.byteLength)
 
-const duplicate = {
+const _duplicate = {
     [types.STRING]: identity,
     [types.NUMBER]: identity,
     [types.BOOLEAN]: identity,
